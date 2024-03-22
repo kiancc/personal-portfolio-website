e4f8005c9e28985c21b046375c4061e57ea1e1de
Kian Chong <kianchristianchong@gmail.com>
2024-03-22 17:16:17 +0000

    altered redirect for about me

--- js/addpost.js
+++ js/addpost.js
@@ -1,5 +1,5 @@
+// code for clearing the add blog form
 document.getElementById('clear').addEventListener('click', clearPost); 
-
 function clearPost(e) {
     // Clear the form fields
     const choice = window.confirm("Are you sure you want to clear?");
@@ -10,3 +10,13 @@ function clearPost(e) {
     }
 }
 
+// code for preventing submission of empty blog form
+document.getElementById('submit').addEventListener('click', preventEmptyPost); 
+function preventEmptyPost(e) {
+    const title = document.getElementById("post-title").value;
+    const text = document.getElementById("blog-text").value;
+    if (title === "" || text === "") {
+        e.preventDefault();
+        window.alert("Looks like the blog is empty!");
+    }
+}
