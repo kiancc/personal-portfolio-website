3ceb80adda95745d2d4f1eb51228682af97d2465
Kian Chong <kianchristianchong@gmail.com>
2024-03-25 19:02:36 +0000

    added css highlighting when blog and text is empty

--- js/addpost.js
+++ js/addpost.js
@@ -15,8 +15,25 @@ document.getElementById('submit').addEventListener('click', preventEmptyPost);
 function preventEmptyPost(e) {
     const title = document.getElementById("post-title").value;
     const text = document.getElementById("blog-text").value;
-    if (title === "" || text === "") {
+    if (title === "" && text === "") {
+        document.getElementById("post-title").style.backgroundColor = "lightcoral";
+        document.getElementById("blog-text").style.backgroundColor = "lightcoral";
         e.preventDefault();
-        window.alert("Looks like the blog is empty!");
+        window.alert("Looks like the title and text are empty!");
+    } else if (title == "") {
+        document.getElementById("post-title").style.backgroundColor = "lightcoral";
+        e.preventDefault();
+        window.alert("Looks like the title is empty!");
+    } else if (text == "") {
+        document.getElementById("blog-text").style.backgroundColor = "lightcoral";
+        e.preventDefault();
+        window.alert("Looks like the text is empty!");
     }
+}
+
+// code for clearing styling if the user enters text after alert
+document.getElementById('post-title').addEventListener('input', clearStyle);
+document.getElementById('blog-text').addEventListener('input', clearStyle);
+function clearStyle(e) {
+    e.target.style.backgroundColor = "white";
 }
