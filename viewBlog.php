60d458de2a2353b7d247ecb4a03f4df7d940e13c
Kian Chong <kianchristianchong@gmail.com>
2024-04-07 17:29:00 +0100

    initial commit

---
+++ viewBlog.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <title>My Homepage</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/form-style.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" media="screen and (max-width: 768px)" href="css/mobile.css">
+</head>
+<body>
+    <header id="title">
+        <a href="index.html"><h1><strong>Kian Christian Chong</strong></h1></a>
+        <nav id="page-navigation">
+            <ul>
+                <li><a href="index.html#about-me">About Me</a></li>
+                <li><a href="education.html">Education</a></li>
+                <li><a href="projects.html">Projects</a></li>
+                <li><a href="experience.html">Experience</a></li>
+                <li><a href="blog.html">Blog</a></li>
+            </ul>
+        </nav>
+    </header>
+
+        <div id="main-content">          
+            <div id="blog-container">
+
+                <article id="blog-title" class="card">
+                    <h1 >Blog</h1>
+                    <aside class="big-button">
+                        <a href="addpost.html">Add Post</a>
+                    </aside>
+                </article>
+
+                <article id="blog-entries">
+                    <!-- PHP code to connect to db and display blog entries -->
+                    <?php
+                        $servername = "127.0.0.1";
+                        $username = "root";
+                        $password = "";
+                        $dbname = "blogentries";
+
+                        // Creates connection
+                        $conn = new mysqli($servername, $username, $password, $dbname);
+
+                        // Checks connection
+                        if ($conn->connect_error) {
+                            die("Connection failed: " . $conn->connect_error);
+                        }
+                        
+                        // grabs entries from db and displays them
+                        $sql = "SELECT * FROM blogentries";
+                        $result = $conn->query($sql);
+                        if ($result->num_rows == 0) {
+                            // redirects to login.html if no entries
+                            header("Location: login.html");
+                            exit;
+                        }
+                        while ($row = $result->fetch_assoc()) {
+                        
+                            echo '<div class="blog-entry">
+                                    <h2><strong>'.$row['title'].'</strong></h2>
+                                    <p id="date">'.$row['dateTime'].'</p>
+                                    <hr>
+                                    <p class="p1">'.$row['text'].'</p>
+                                </div>';
+                        }
+                        $conn->close();
+                    ?>
+                </article>
+            </div>
+        </div>
+        <footer id="footer">
+            <section id="signature">
+                <p>Kian Chong 2024 &copy;</p>
+            </section>
+        </footer>
+    </div>
+</body>
+</html>
