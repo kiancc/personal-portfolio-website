5b9e311949d2fa5439789e8d4f7d47940038ba4f
Kian Chong <kianchristianchong@gmail.com>
2024-04-07 17:33:14 +0100

    updated redirect link when submitting post

--- addpost.php
+++ addpost.php
@@ -6,36 +6,35 @@
     <title>Document</title>
 </head>
 <body>
-<?php
-    $servername = "127.0.0.1";
-    $username = "root";
-    $password = "";
-    $dbname = "blogentries";
+    <?php
+        $servername = "127.0.0.1";
+        $username = "root";
+        $password = "";
+        $dbname = "blogentries";
 
-    // Creates connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
+        // Creates connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Checks connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $text = $_POST["blog-text"];
-        $title = $_POST["title"];
-        $time = date("Y-m-d H:i:s");
-        $sql = "INSERT INTO blogentries (title, text, dateTime)
-        VALUES ('$title', '$text', '$time')";
-        if ($conn->query($sql) === TRUE) {
-            // redirects to viewblog
-            header("Location: blog.html");
-            exit;
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
+        // Checks connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
         }
-        $conn->close();
-    }
 
-?>
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $text = $_POST["blog-text"];
+            $title = $_POST["title"];
+            $time = date("Y-m-d H:i:s");
+            $sql = "INSERT INTO blogentries (title, text, dateTime)
+            VALUES ('$title', '$text', '$time')";
+            if ($conn->query($sql) === TRUE) {
+                // redirects to viewblog
+                header("Location: viewBlog.php");
+                exit;
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+            $conn->close();
+        }
+    ?>
 </body>
 </html>
